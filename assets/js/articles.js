37bf091d3218b8a326fd730890201c06cd38bf3a
JJ Ashcraft <jj@jjashcraft.com>
2018-03-09 11:34:51 -0500

    changed rss feed js

--- assets/js/articles.js
+++ assets/js/articles.js
@@ -1,16 +1,20 @@
-$(function() {
-        var $content = $('#content');
-        var data = {
-            rss_url: 'https://www.medium.com/feed/@jjashcraft'
-        };
-$.get('https://api.rss2json.com/v1/api.json', data, function(response) {
-            if (response.status == 'ok') {
-                var output = '<h1>' + response.feed.title + '</h1>';
-$.each(response.items, function(k, item) {
-                    output += '<h2><a href="' + item.link + '" >' + item.title + '</h2></a>';
-                    output += '<p>' + item.description + '</p>';
-                });
-$content.html(output);
-}
-});
+$.ajax({
+        url: 'https://api.rss2json.com/v1/api.json',
+        method: 'GET',
+        dataType: 'json',
+        data: {
+            rss_url: 'https://www.medium.com/feed/@jjashcraft',
+            api_key: 'daaojcgetrusjckgyl79sstz3fcg6hpny06ukdis', // put your api key here
+            count: 2
+        }
+}).done(function (response) {
+    if(response.status != 'ok'){ throw response.message; }
+
+    console.log('====== ' + response.feed.title + ' ======');
+
+    for(var i in response.items){
+        var item = response.items[i];
+        console.log(item.title);
+
+    }
 });
