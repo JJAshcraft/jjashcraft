4019c06b82572b8aa7ea2049b4a50a1ae2927af3
JJ Ashcraft <jj@jjashcraft.com>
2018-03-11 09:37:37 -0400

    updates static nav

--- assets/js/articles2.js
+++ assets/js/articles2.js
@@ -1,44 +1,45 @@
-
 var content = document.getElementById('content');
 var sidebar = document.getElementById('sidebar');
 
 $.ajax({
-        url: 'https://api.rss2json.com/v1/api.json',
-        method: 'GET',
-        dataType: 'json',
-        data: {
-            rss_url: 'https://www.medium.com/feed/@jjashcraft',
-            api_key: 'daaojcgetrusjckgyl79sstz3fcg6hpny06ukdis', // put your api key here
-            count: 5
-        }
-}).done(function (response) {
-    if(response.status != 'ok'){ throw response.message; }
+  url: 'https://api.rss2json.com/v1/api.json',
+  method: 'GET',
+  dataType: 'json',
+  data: {
+    rss_url: 'https://www.medium.com/feed/@jjashcraft',
+    api_key: 'daaojcgetrusjckgyl79sstz3fcg6hpny06ukdis', // put your api key here
+    count: 5
+  }
+}).done(function(response) {
+  if (response.status != 'ok') {
+    throw response.message;
+  }
 
-    console.log('====== ' + response.feed.title + ' ======');
+  console.log('====== ' + response.feed.title + ' ======');
 
-    var output = '';
-    var sideOutput = '';
+  var output = '';
+  var sideOutput = '';
 
-    for(var i in response.items){
-            var item = response.items[i];
-            console.log(item.title);
+  for (var i in response.items) {
+    var item = response.items[i];
+    console.log(item.title);
 
-            // output += '<div ' + ' id="' + [i] + '">'
-            // output += '<a class="anchor" id="top"></a>'
+    // output += '<div ' + ' id="' + [i] + '">'
+    // output += '<a class="anchor" id="top"></a>'
 
-        output += '<div>'
-        output += '<a class="anchor" id="' + [i] + '"></a>'
-        output += '<p><h2>' +
-        item.title + '</h2></p>';
-        output += '<p><h5>' +
-        item.pubDate + '</h5></p>';
+    output += '<div>'
+    output += '<a id="' + [i] + '"></a>'
+    output += '<hr class="anchor"><p><h2>' +
+      item.title + '</h2></p>';
+    output += '<p><h5>' +
+      item.pubDate + '</h5></p>';
 
-        output += item.content;
+    output += item.content;
 
-        sideOutput += '<p><a href="' + '#' + [i] + '"><h5>' + '<i class="fas fa-file-alt"></i> - ' +
-        item.title + '</h5></a></p><hr>';
-      }
+    sideOutput += '<p><a href="' + '#' + [i] + '"><h5>' + '<i class="fas fa-file-alt"></i> - ' +
+      item.title + '</h5></a></p><hr>';
+  }
 
-content.innerHTML = output;
-sidebar.innerHTML = sideOutput;
+  content.innerHTML = output;
+  sidebar.innerHTML = sideOutput;
 });
