8be897dc0e3be8630c62d1a2672a20873332558c
JJ Ashcraft <jj@jjashcraft.com>
2018-03-16 09:20:20 -0400

    learned about isotope filter, added with jquery to portfolio.

---
+++ assets/js/gallery.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+  var $grid = $('.grid').isotope({
+    // options
+    itemSelector: '.portfolio-item',
+  });
+
+
+  $('.filters-button-group').on('click', 'button', function() {
+    var filterValue = $(this).attr('data-filter');
+    // use filterFn if matches value
+
+    $grid.isotope({
+      filter: filterValue
+    });
+
+
+  });
+
+  $('.allButtons').on('click', '.btn', function() {
+    $(this).removeClass('btn-dark').addClass('btn-light').siblings().removeClass('btn-light').addClass('btn-dark');
+  });
+
+
+  // Your code
+});
